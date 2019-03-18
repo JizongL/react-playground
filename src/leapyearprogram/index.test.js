807af5f55cd6c889f03e4f3360f1e4d993bb1e31
JizongL <2015rpro@gmail.com>
2019-03-18 16:19:14 -0400

    added Ject testing

---
+++ src/leapyearprogram/index.test.js
@@ -0,0 +1,37 @@
+import leapYear  from './index';
+
+it('should NOT be a leap year if divisible by 100, not 400', () => {
+  const input = 1900;
+  const expectedOutput = false; 
+  const actualOutput = leapYear(input);
+  expect(actualOutput).toBe(expectedOutput);
+});
+
+it('should Not be a leap year if not divisible by 400', () => {
+  const input = 2000;
+  const expectedOutput = true; 
+  const actualOutput = leapYear(input);
+  expect(actualOutput).toBe(expectedOutput);
+});
+
+it('should Not be a leap year if not divisible by 4, not 100, leap year',()=>{
+  const input = 1984;
+  const expectedOutput = true;
+  const actualoutput = leapYear(input);
+  expect(expectedOutput).toBe(actualoutput);
+});
+
+it('should Not be a leap year if not divisible by 4',()=>{
+  const input = 1983;
+  const expectedOutput = false;
+  const actualOutput = leapYear(input)
+  expect(expectedOutput).toBe(actualOutput)
+});
+
+it('should throw an error for year before 1582',()=>{
+  expect(()=>{
+    leapYear(1582);
+  }
+  ).toThrow();
+});
+
